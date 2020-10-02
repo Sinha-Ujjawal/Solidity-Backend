0254c15444bc4c81c079ec92045dcbc6ede64d99
kingZthefirst-cypher <ujjwaluplay@gmail.com>
2020-10-02 17:18:15 +0530

    Using bulk create instead of create

--- src/qa/services.py
+++ src/qa/services.py
@@ -7,21 +7,23 @@ from .models import *
 def create_quiz(*, user: User, quiz_questions: List[Dict[str, str]]):
     quiz = Quiz.objects.create(user_id=user, room_id=secrets.token_urlsafe(10))
 
-    for quiz_question in quiz_questions:
-        question = Question.objects.create(
-            quiz_id=quiz, question=quiz_question["question"]
-        )
+    questions = Question.objects.bulk_create(
+        [
+            Question(quiz_id=quiz, question=quiz_question["question"])
+            for quiz_question in quiz_questions
+        ]
+    )
 
-        answer = quiz_question["answer"]
-        choices = quiz_question["choices"]
-
-        assert answer in choices
-
-        Option.objects.bulk_create(
-            [
-                Option(question_id=question, option=choice, is_answer=choice == answer)
-                for choice in choices
-            ]
-        )
+    Option.objects.bulk_create(
+        [
+            Option(
+                question_id=question,
+                option=choice,
+                is_answer=choice == quiz_question["answer"],
+            )
+            for quiz_question, question in zip(quiz_questions, questions)
+            for choice in quiz_question["choices"]
+        ]
+    )
 
     return quiz.room_id
