8cf28910eac5867f862deee294727ec2b09d3d9e
kingZthefirst-cypher <ujjwaluplay@gmail.com>
2020-10-02 16:31:47 +0530

    Bug fix for creating orm objects

--- src/qa/services.py
+++ src/qa/services.py
@@ -5,24 +5,25 @@ from .models import *
 
 
 def create_quiz(*, user: User, quiz_questions: List[Dict[str, str]]):
-    quiz = Quiz(user_id=user, room_id=secrets.token_bytes(16))
+    quiz = Quiz.objects.create(user_id=user, room_id=secrets.token_bytes(16))
 
-    questions = []
-    options = []
     for quiz_question in quiz_questions:
-        question = Question(quiz_id=quiz.id, question=quiz_question["question"])
+        question = Question.objects.create(
+            quiz_id=quiz.id, question=quiz_question["question"]
+        )
+
         answer = quiz_question["answer"]
         choices = quiz_question["choices"]
+
         assert answer in choices
-        for choice in choices:
-            option = Option(
-                question_id=question.id, option=choice, is_answer=choice == answer
-            )
-            options.append(option)
-        questions.append(question)
 
-    Question.objects.bulk_create(questions)
-    Option.objects.bulk_create(options)
+        Option.objects.bulk_create(
+            [
+                Option(
+                    question_id=question.id, option=choice, is_answer=choice == answer
+                )
+                for choice in choices
+            ]
+        )
 
-    quiz.save()
     return quiz.room_id
